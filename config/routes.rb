5929f2ad0e6c1833eed7652d2c46392c81bdfdaa
瀧澤匠子 <sthkm1111@gmail.com>
2026-01-11 15:45:45 +0900

    fix: topページ変更、reactをファイル修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  root "pages#index"
+
+  root "static_pages#top"
+
+  get "/dashboard", to: "dashboard#index"
+
+  authenticated :user do
+    root to: "dashboard#index", as: :authenticated_root
+  end
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  get "/dashboard", to: "dashboard#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
