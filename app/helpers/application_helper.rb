95fc3cd76f465527b92f9f573f59721f4d554dce
瀧澤匠子 <sthkm1111@gmail.com>
2026-01-22 15:18:32 +0900

    add: ログイン機能にフラッシュメッセージ実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # Flashメッセージの種類に応じたCSSクラスを返す
+  def flash_class_for(type)
+    case type.to_s
+    when "notice"
+      "success"
+    when "alert"
+      "error"
+    else
+      type.to_s
+    end
+  end
 end
