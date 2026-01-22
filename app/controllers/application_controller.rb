5f52f0207cca87d0b36c0dddbba746ff1f659f7e
瀧澤匠子 <sthkm1111@gmail.com>
2026-01-22 23:44:19 +0900

    fix: coderabbit指摘事項修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :devise_controller?
 
   protected
 
