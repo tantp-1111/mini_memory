a7879157925e00d4cb0c85080f9c4eeff591f853
瀧澤匠子 <sthkm1111@gmail.com>
2026-03-23 00:37:36 +0900

    add: OGP設定追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,30 @@ module ApplicationHelper
       type.to_s
     end
   end
+
+  def default_meta_tags
+    {
+      site: 'minimemory',
+      title: '捨てられない思い出を記録するアプリ',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'minimemoryを使えば、捨てられない小さなモノの大切な思い出を簡単に記録して共有できます。',
+      keywords: '思い出,記録,共有,こども,家族',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image', # Xで表示する場合は大きいカードを使用
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
