83cc2505ed68480ec1407d8a5e5b000d5189c5a7
瀧澤匠子 <sthkm1111@gmail.com>
2026-03-23 12:34:05 +0900

    fix: coderabbit指摘事項修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,21 +14,21 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: "minimemory",
-      title: "捨てられない思い出を記録するアプリ",
+      title: (content_for?(:title) ? content_for(:title) : "捨てられない思い出を記録するアプリ"),
       reverse: true,
       charset: "utf-8",
       description: "minimemoryを使えば、捨てられない小さなモノの大切な思い出を簡単に記録して共有できます。",
       keywords: "思い出,記録,共有,こども,家族",
-      canonical: request.original_url,
+      canonical: "#{request.base_url}#{request.path}",
       separator: " | ",
       og: {
         site_name: :site,
         title: :title,
         description: :description,
         type: "website",
-        url: request.original_url,
+        url: url: "#{request.base_url}#{request.path}",
         image: image_url("ogp.png"),
-        local: "ja-JP"
+        locale: "ja-JP"
       },
       twitter: {
         card: "summary_large_image", # Xで表示する場合は大きいカードを使用
