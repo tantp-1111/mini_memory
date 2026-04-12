a3b1fcb3a6960ac06e086efcbf8c77c0dabeb4cc
瀧澤匠子 <sthkm1111@gmail.com>
2026-04-13 00:14:08 +0900

    db:migrate完了

--- db/migrate/20260412145726_create_user_family_groups.rb
+++ db/migrate/20260412145726_create_user_family_groups.rb
@@ -1,13 +1,11 @@
 class CreateUserFamilyGroups < ActiveRecord::Migration[7.2]
   def change
     create_table :user_family_groups do |t|
-      t.bigint "user_id", null: false
-      t.bigint "family_group_id", null: false
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.integer "role", default: 4, null: false
-      t.boolean "is_admin", default: false, null: false
+      t.references :user, null: false, foreign_key: true
+      t.references :family_group, null: false, foreign_key: true
+      t.integer :role, null: false, default: 1  # 0: owner, 1: member
       t.timestamps
     end
+    add_index :user_family_groups, [:user_id, :family_group_id], unique: true
   end
 end
