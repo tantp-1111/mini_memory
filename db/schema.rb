f097371903122c66f4cc817c738879266cb9b80a
瀧澤匠子 <sthkm1111@gmail.com>
2026-06-05 09:41:58 +0900

    add: User に admin 列を追加する
    rails_admin の認可判定に使う boolean カラム。default: false / null: false で
    既存ユーザーは全員 admin: false で埋まる。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_05_24_073155) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_05_004121) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -147,6 +147,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_05_24_073155) do
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
